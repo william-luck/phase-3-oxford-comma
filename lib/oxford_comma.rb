ee8387cd96ebabf87ce3948da0196161602d0a71
William Luck <luck.william.h@gmail.com>
2022-10-19 10:02:12 -0300

    commit tree e3de74ab5b3d1620d1800b054f940a41a3bc4d89

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def oxford_comma(array)
 
+    if array.length == 1
+        array.join
+    elsif array.length == 2 
+        array.join(' and ')
+    else
+        array[-1] = "and #{array[-1]}"
+        array.join(', ')
+    end
+    
 end
+
+oxford_comma(['hello','is i', 'come to see you'])
+
+# binding.pry
+# 0
